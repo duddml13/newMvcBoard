bf892c5cee37c8dac90f0ba01fc20557aa959366
duddml13 <37687335+duddml13@users.noreply.github.com>
2018-03-23 17:23:16 +0900

    ver0.1
    처음올리는 프로젝트

---
+++ OpenProject/src/main/java/com/bitcamp/open/member/service/MemberListService.java
@@ -0,0 +1,32 @@
+package com.bitcamp.open.member.service;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bitcamp.open.member.dao.LoginDao;
+import com.bitcamp.open.member.dao.MemberDao;
+import com.bitcamp.open.member.dao.MemberMybatisDao;
+import com.bitcamp.open.member.model.Member;
+
+public class MemberListService {
+	// @Autowired
+	// private MemberMybatisDao dao;
+	// private MemberDao dao;
+
+	@Autowired
+	private SqlSessionTemplate sqlSessionTemplate;
+
+	private LoginDao dao;
+
+	public List<Member> listview() {
+
+		dao = sqlSessionTemplate.getMapper(LoginDao.class);
+
+		// 자바의 컬렉션 : 객체 저장,List의 경우 저장 순서를 가진다, for문을 이용해서 전체 참조
+		List<Member> memberlist = dao.selectList();
+		return memberlist;
+	}
+
+}
